e16dd011fc7da361f4c03c59482ff0c76b27374b
Gustavo Bastian <bastian.gustavo@gmail.com>
2022-09-16 14:29:50 -0300

    endpoint for asking how many patients are in each treatment(for statistics)

--- src/backend/routes/Statistics/index.js
+++ src/backend/routes/Statistics/index.js
@@ -27,5 +27,21 @@ routerStatistics.get('/promPatient', function(req, res) {
         res.send(result).status(202);
  });
 });
+/**
+ * Send to client events counts by patient
+ * PatientSpecTable.specId
+*/
+routerStatistics.get('/promTreatment', function(req, res) {    
+    console.log("asking for list of treatment")
+    pool.query('SELECT SpecTable.name,COUNT(*) as cn FROM `PatientSpecTable` \
+    JOIN SpecTable on SpecTable.id=`PatientSpecTable`.`specId`\
+    GROUP BY `PatientSpecTable`.`specId` ASC ', function(err, result, fields) {
+        if (err) {
+            res.send(err).status(400);
+            return;
+        }
+        res.send(result).status(202);
+ });
+});
 
  module.exports = routerStatistics;
